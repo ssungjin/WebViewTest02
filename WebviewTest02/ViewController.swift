fba21df74a84cb949c6d76818248ffab7b833123
201631053 <hubot@githhub@com>
2019-09-09 16:31:09 +0900

    dd

--- WebviewTest02/ViewController.swift
+++ WebviewTest02/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate,WKNavigationDelegate {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var forwardbutton: UIBarButtonItem!
+    @IBOutlet var BackButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // UISearchBarDelegate 객체와 UIViewController 객체와 연결
+        searchBar.delegate = self
+        webView.navigationDelegate = self
+        let request = URLRequest(url: URL(string: "http://www.google.com")!)
+        webView.load(request)
     }
-
-
+    
+    func backAction(_ sender: Any) {
+        if self.webView.canGoBack {
+            self.webView.goBack()
+        }
 }
 
+    func forwardAction(_ sender: Any) {
+        if self.webView.canGoForward {
+            self.webView.goForward()
+        }
+}
+    // SearchBarDelegate Method 호출
+    // called when keyboard search button pressed
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let inputURL = searchBar.text
+        let request = URLRequest(url: URL(string: inputURL!)!)
+        webView.load(request)
+}
+    //웹 받기 시작할때
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+}
+    //웹 받는 것 끝났을때
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+      
+        BackButton.isEnabled = webView.canGoBack
+        forwardbutton.isSpringLoaded = webView.canGoForward
+}
+}
